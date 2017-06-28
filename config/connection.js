f3cc951f58dbf8c8a925ae7374435707659fd49a
Randy Lam <randylam87@gmail.com>
2017-06-27 18:33:07 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,27 @@
 const Sequelize = require('sequelize');
-let connection = new Sequelize('burgers_db', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-  port: 8889
-});
+// let connection = new Sequelize('burgers_db', 'root', 'root', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   },
+//   port: 8889
+// });
+let connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    // connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = new Sequelize(process.env[JAWSDB_URL])
 } else {
-connection
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-}
+    connection
+        .authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
+    }
 module.exports = connection;
