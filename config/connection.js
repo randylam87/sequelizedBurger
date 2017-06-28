15b1c0d569210574cc92fa0ab06d4edf9badcec9
Randy Lam <randylam87@gmail.com>
2017-06-27 18:36:03 -0700

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 const Sequelize = require('sequelize');
-// let connection = new Sequelize('burgers_db', 'root', 'root', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   },
-//   port: 8889
-// });
-let connection;
+let connection = new Sequelize('burgers_db', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  port: 8889
+});
 
 if (process.env.JAWSDB_URL) {
     // connection = mysql.createConnection(process.env.JAWSDB_URL);
-    connection = new Sequelize(process.env[JAWSDB_URL])
+    connection = new Sequelize(process.env.JAWSDB_URL)
 } else {
     connection
         .authenticate()
